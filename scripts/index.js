dcaf4ed99d6c029f2c8e48e0c3a98eb054e80f89
Irina Ermoeva <evamoer@yandex.ru>
2021-06-26 16:07:33 +0500

    feat:добавить вывод карточек в галерее через template

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,58 @@ const popupOpenButtonElement = document.querySelector('.button_type_edit');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.button_type_close');
 // находим форму в DOM
-let formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form');
 // находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input_type_profile-name');
-let jobInput = formElement.querySelector('.popup__input_type_profile-about');
+const nameInput = formElement.querySelector('.popup__input_type_profile-name');
+const jobInput = formElement.querySelector('.popup__input_type_profile-about');
 // выбираем элементы из профиля, которые должны измениться
-let profileInfo = document.querySelector('.profile__info')
-let profileName = profileInfo.querySelector('.profile__name');
-let profileAbout = profileInfo.querySelector('.profile__about');
+const profileInfo = document.querySelector('.profile__info')
+const profileName = profileInfo.querySelector('.profile__name');
+const profileAbout = profileInfo.querySelector('.profile__about');
+// массив с карточками для галереи
+const items = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const galleryTemplateContent = document.querySelector('.gallery-item-template').content;
+const galleryListElement = document.querySelector('.gallery-table');
+
+function renderItem(item) {
+  const galleryItemElement = galleryTemplateContent.cloneNode(true);
+  const galleryItemElementText = galleryItemElement.querySelector('.card__title');
+  const galleryItemImage = galleryItemElement.querySelector('.card__image');
+  galleryItemImage.src = item.link;
+  galleryItemImage.alt = item.name;
+  galleryItemElementText.textContent = item.name;
+  galleryListElement.append(galleryItemElement);
+};
+
+function renderItems(items) {
+  items.forEach(renderItem);
+};
 
 //функция для открытия попапа по клику
 const openPopup = function() {
@@ -26,7 +70,7 @@ const closePopup = function() {
 
 // обработчик «отправки» формы
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки.
+    evt.preventDefault(); // эта строчка отменяет стандартную отправку формы, так мы можем определить свою логику отправки
     // вставляем новые значения из формы в элементы профиля с помощью textContent
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
@@ -39,3 +83,5 @@ formElement.addEventListener('submit', formSubmitHandler);
 //слушатели клика по кнопке для функций открытия-закрытия попапа
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
+
+renderItems(items);
