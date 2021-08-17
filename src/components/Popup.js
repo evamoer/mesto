253a6f61c34813e4b549567bca9771605c43141a
Irina Ermoeva <evamoer@gmail.com>
2021-08-17 11:39:19 +0500

    fix: add correction to Popup class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,12 +6,16 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose);
+    document.addEventListener('keydown', (evt) => {
+      this._handleEscClose(evt);
+    });
   }
 
   close() {
     this._popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose);
+    document.removeEventListener('keydown', (evt) => {
+      this._handleEscClose(evt);
+    });
   }
 
   handleOverlayClick(evt) {
